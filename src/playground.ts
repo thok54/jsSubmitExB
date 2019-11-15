1c3aab011f7d8c310f2f7f709bf77c802ccf71d8
Jesus Alvarez <thok54@hotmail.com>
2019-11-15 14:33:28 +0100

    first commit

---
+++ src/playground.ts
@@ -0,0 +1,164 @@
+
+//****   1.Aplanando Arrays          ***///
+console.log("*** Exercise 1: Aplanando Arrays ***");
+
+//Apartado A & B
+console.log("Apartado A & B");
+
+const sample: any[] = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+console.log(sample);
+
+const flatten: any = (ary: Array<number>) => ary.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
+console.log(flatten(sample));
+
+
+
+
+
+// ****   2.Acceso en profundidad          ***///
+console.log("*** 2.Acceso en profundidad  ***");
+
+//Apartado A
+console.log("Apartado A");
+
+const myObject = {
+    a: 1,
+    b: {
+        c: null,
+        d: {
+            e: 3,
+            f: {
+                g: "bingo",
+            }
+        }
+    }
+};
+
+
+const deepGet = (nestedObj, ...pathArr) => {
+    return pathArr.reduce((obj, key) =>
+        (obj && obj[key] !== 'undefined') ? obj[key] : undefined, nestedObj);
+}
+
+console.log(deepGet(myObject, "x"));
+console.log(deepGet(myObject, "a"));
+console.log(deepGet(myObject, "b"));
+console.log(deepGet(myObject, "b", "c"));
+console.log(deepGet(myObject, "b", "d", "f", "g"));
+console.log(deepGet(myObject));
+
+
+
+//Apartado B
+console.log("");
+console.log("Apartado B");
+
+let deepSet = (value, obj, ...pathArr) => {
+
+    if (pathArr.length > 1) {
+        if (!obj.hasOwnProperty(pathArr[0]) || typeof obj[pathArr[0]] !== "object") obj[pathArr[0]] = {}
+
+        return deepSet(pathArr.slice(1), value, obj[pathArr[0]])
+
+    } else {
+        obj[pathArr[0]] = value
+        return true
+    }
+}
+
+
+
+deepSet(1, myObject, "a", "b");
+console.log(JSON.stringify(myObject));
+deepSet(2, myObject, "a", "c");
+console.log(JSON.stringify(myObject));
+deepSet(3, myObject, "a");
+console.log(JSON.stringify(myObject));
+deepSet(4, myObject);
+console.log(JSON.stringify(myObject));
+
+
+
+
+
+
+// ****   3.Arbol          ***///
+console.log("");
+console.log("*** 3.Arbol  ***");
+
+type tree = {
+    node: any;
+    sons: tree[];
+}
+
+const tree1: tree = { node: 1, sons: [{ node: 2, sons: [] }, { node: 3, sons: [{ node: 4, sons: [] }] }] };
+console.log(tree1);
+
+
+
+// ****   4.Trazas por consola          ***///
+console.log("");
+console.log("*** 4.Trazas por consola  ***");
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const showMessage = async ([time, message]) => {
+    await delay(time);
+    console.log(message);
+};
+
+const triggers = [
+    async () => await showMessage([200, "third"]),
+    async () => await showMessage([100, "second"])
+];
+
+async function run(triggers) {
+    for (const t of triggers) {
+        await t();
+    }
+    console.log("first");
+}
+
+run(triggers);
+
+
+
+// ****   5.Memoizacion          ***///
+console.log("");
+console.log("*** 5.Memorizacion  ***");
+//Apartado A & B
+console.log("Apartado A y B");
+
+const expensiveFunction = () => {
+    console.log("Una única llamada");
+    return 3.1415;
+}
+
+const memoize = func => func();
+
+const memoized = memoize(expensiveFunction);
+
+console.log(memoized);
+console.log(memoized);
+console.log(memoized);
+
+
+//Apartado C
+console.log("");
+console.log("Apartado C");
+
+
+let count = 0;
+
+const repeatText = (repetitions: number, text: string): string =>
+    (count++ , `${text}`.repeat(repetitions).trim());
+
+const memoizeC = func => func;
+
+const memoizedGreet = memoizeC(repeatText);
+
+console.log(memoizedGreet(1, "pam"));
+console.log(memoizedGreet(3, "chun"));
+console.log(memoizedGreet(1, "pam"));
+console.log(memoizedGreet(3, "chun"));
+console.log(count);
